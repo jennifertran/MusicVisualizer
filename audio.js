194ee0b02c5db799f863f0076a74d61099150fba
Justin Gouge <justingouge77@yahoo.ca>
2017-12-16 01:02:38 -0600

    Just made it so you can play again when play is going

--- audio.js
+++ audio.js
@@ -5,7 +5,7 @@ var audioSrc;
 var analyser;
 var bufferLength;
 var dataArray; // frequency data
-var state = 2; //0 for playing, 1 for paused, 2 for stopped 
+var state = 2; //0 for playing, 1 for paused, 2 for stopped
 
 function selectMusic(e) {
   musicFiles = e.target.files;
@@ -17,32 +17,37 @@ function getFreq() {
 }
 
 function play() {
-  var num = Math.floor(Math.random() * musicFiles.length);
-  var musicFile = URL.createObjectURL(musicFiles[num]);
-  $("#music").attr("src", musicFile);
-  document.getElementById('music').play();
 
-  if (!audio) {
-    audio = document.getElementById('music');
-    audio.crossOrigin = "anonymous";
-    audioSrc = audioCtx.createMediaElementSource(audio);
-    analyser = audioCtx.createAnalyser();
-    analyser.fftSize = 256;
-    audioSrc.connect(analyser);
-    audioSrc.connect(audioCtx.destination);
-    bufferLength = analyser.frequencyBinCount;
-    dataArray = new Uint8Array(bufferLength);
+  if(state != 0){
+    var num = Math.floor(Math.random() * musicFiles.length);
+    var musicFile = URL.createObjectURL(musicFiles[num]);
+    $("#music").attr("src", musicFile);
+    document.getElementById('music').play();
+
+    if (!audio) {
+      audio = document.getElementById('music');
+      audio.crossOrigin = "anonymous";
+      audioSrc = audioCtx.createMediaElementSource(audio);
+      analyser = audioCtx.createAnalyser();
+      analyser.fftSize = 256;
+      audioSrc.connect(analyser);
+      audioSrc.connect(audioCtx.destination);
+      bufferLength = analyser.frequencyBinCount;
+      dataArray = new Uint8Array(bufferLength);
+    }
+    state = 0;
   }
 
 }
 
 function stop() {
   document.getElementById('music').pause();
+  state = 2;
 }
 
 function pause(){
-
-
+  document.getElementById('music').pause();
+  state = 1;
 }
 
 window.onload = function (e) {
